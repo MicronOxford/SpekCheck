69b783e7cd0dfcfb6e3cdf04178f2662b3417906
Mick <mick.phillips@gmail.com>
2017-10-10 14:56:11 +0100

    Typos.

--- spectral-transmission.js
+++ spectral-transmission.js
@@ -469,15 +469,15 @@ function drawPlot(dye, excitation, filters, filterModes) {
 
     if (t_eff != null && e_eff != null && bright != null) {
        CHART.options.title = {display: true,
-                               text: 'Efficency: ex ' + (100*e_eff).toFixed(1) + '%, em ' + (100*t_eff).toFixed(1) + '%' + ', brightness ' + bright.toFixed(2),
+                               text: 'Efficiency: ex ' + (100*e_eff).toFixed(1) + '%, em ' + (100*t_eff).toFixed(1) + '%' + ', brightness ' + bright.toFixed(2),
                                fontSize: 24};
     } else if (t_eff != null && e_eff != null) {
         CHART.options.title = {display: true,
-                               text: 'Efficency: ex ' + (100*e_eff).toFixed(1) + '%, em ' + (100*t_eff).toFixed(1) + '%',
+                               text: 'Efficiency: ex ' + (100*e_eff).toFixed(1) + '%, em ' + (100*t_eff).toFixed(1) + '%',
                                fontSize: 24};
     } else if (t_eff != null) {
         CHART.options.title = {display: true,
-                               text: 'Efficency:  ' + (100*t_eff).toFixed(1) + '%',
+                               text: 'Efficiency:  ' + (100*t_eff).toFixed(1) + '%',
                                fontSize: 24};
     } else {
         CHART.options.title = {display: false,
