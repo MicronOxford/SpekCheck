8263e1e1547be2114bf90e16d5f17bdac85dc48e
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2017-12-15 11:25:06 +0000

    added script to update index files in dyes, filters and excitation directories

---
+++ update-index-files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#  * Spectral Transmission tool
+#  *
+#  * Copyright 2017 Mick Phillips (mick.phillips@gmail.com)
+#  * and Ian Dobbie (ian.dobbie@gmail.com)
+#  *
+#  * This program is free software: you can redistribute it and/or modify
+#  * it under the terms of the GNU General Public License as published by
+#  * the Free Software Foundation, either version 3 of the License, or
+#  * (at your option) any later version.
+#  *
+#  * This program is distributed in the hope that it will be useful,
+#  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  * GNU General Public License for more details.
+#  *
+#  * You should have received a copy of the GNU General Public License
+#  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Shell script to update the dye, filter and excitation index files.
+#
+
+# loop over dyes, filters and excitation directories
+for dir in dyes filters excitation; do
+    #cd to directory, update index.html
+    (cd $dir; ls -1 > index.html; echo $dir) ;
+done
