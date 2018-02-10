5f6a49ae30d3f71b29035db3389413bfb318541f
Carnë Draug <carandraug+dev@gmail.com>
2018-02-10 16:04:38 +0000

    maint: mention SpekCheck on GPL header as recommended by FSF

--- js/spekcheck.js
+++ js/spekcheck.js
@@ -3,18 +3,18 @@
  *
  * Copyright 2017 Mick Phillips (mick.phillips@gmail.com)
  *
- * This program is free software: you can redistribute it and/or modify
+ * SpekCheck is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * SpekCheck is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with SpekCheck.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Extensions to strip from source filenames, and files to exclude.
--- src/create-spectrum.py
+++ src/create-spectrum.py
@@ -4,18 +4,18 @@
 ## Copyright (C) 2011 William Krekeler <WKrekeler@cleanearthtech.com>
 ## Copyright (C) 2012-2107 Carnë Draug <carandraug+dev@gmail.com>
 ##
-## Microscope is free software: you can redistribute it and/or modify
+## SpekCheck is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
 ##
-## Microscope is distributed in the hope that it will be useful,
+## SpekCheck is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with Microscope.  If not, see <http://www.gnu.org/licenses/>.
+## along with SpekCheck.  If not, see <http://www.gnu.org/licenses/>.
 
 ## This code is based on the function wavelength2rgb part of the
 ## Octave image package released under GPLv3+, hence the copyright to
